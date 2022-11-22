8723e85b2fb099985c44627d3215517d113e578d
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-11-22 13:35:40 +0900

    refactor: modularize by feature
    Signed-off-by: Yi Byeongyong <ybyo@yibyeongyong.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { EmailService } from './email/email.service';
+import { UsersModule } from './users/users.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
-  controllers: [UsersController],
-  providers: [UsersService, EmailService],
+  imports: [UsersModule, EmailModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
