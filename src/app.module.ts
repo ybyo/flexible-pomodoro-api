935587458252138ba73c8b1ac7adc575b010a5d9
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-12-26 23:45:38 +0900

    refactor: refactor env path base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,24 @@
-import { HttpModule } from '@nestjs/axios';
-import { Module } from '@nestjs/common';
-import { TerminusModule } from '@nestjs/terminus';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
-
-import authConfig from './config/authConfig';
-import emailConfig from './config/emailConfig';
-
 import { validationSchema } from './config/validationSchema';
-import { ExceptionModule } from './exception/exception-module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TerminusModule } from '@nestjs/terminus';
+import { Module } from '@nestjs/common';
 import { LoggingModule } from './logging/logging.module';
+import { HttpModule } from '@nestjs/axios';
 import { HealthCheckController } from './health-check/health-check.controller';
+import { ExceptionModule } from './exception/exception-module';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import authConfig from './config/authConfig';
 import * as path from 'path';
+
+const envPath = path.join(process.cwd(), `env/.${process.env.NODE_ENV}.env`);
 
 @Module({
   imports: [
     UsersModule,
     ConfigModule.forRoot({
-      envFilePath: [
-        path.join(__dirname, `../env/.${process.env.NODE_ENV}.env`),
-      ],
+      envFilePath: [envPath],
       load: [emailConfig, authConfig],
       isGlobal: true,
       validationSchema,
