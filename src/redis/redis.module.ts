df7918e362bdfe9afdce950c4d023541b4e79055
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-01-07 18:56:53 +0900

    Add redis for session management

---
+++ src/redis/redis.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import * as Redis from 'redis';
+
+import { REDIS } from './redis.constants';
+
+// 'rediss://username:password@your.redis.url'
+const url = 'redis://localhost:6379';
+
+@Module({
+  providers: [
+    {
+      provide: REDIS,
+      // useValue: Redis.createClient({ url, legacyMode: true }),
+      useFactory: async () => {
+        const client = Redis.createClient({
+          url: url,
+          legacyMode: true,
+        });
+        await client.connect();
+        return client;
+      },
+    },
+  ],
+  exports: [REDIS],
+})
+export class RedisModule {}
