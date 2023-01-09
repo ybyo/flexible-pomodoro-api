86850b1f81bccd3340fec71fd1e0dff5868ce869
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-01-09 16:51:45 +0900

    Updating configuration for development

--- src/config/accessTokenConfig.ts
+++ src/config/accessTokenConfig.ts
@@ -2,11 +2,8 @@ import { registerAs } from '@nestjs/config';
 import { CookieOptions } from 'express';
 
 export default registerAs<CookieOptions>('accessToken', () => ({
-  expires: new Date(
-    Date.now() + parseInt(process.env.ACCESS_TOKEN_LIFETIME) * 60 * 1000,
-  ),
   maxAge: parseInt(process.env.ACCESS_TOKEN_LIFETIME) * 60 * 1000,
   httpOnly: true,
-  secure: true,
+  secure: false,
   sameSite: 'none',
 }));
--- src/config/refreshTokenConfig.ts
+++ src/config/refreshTokenConfig.ts
@@ -2,11 +2,8 @@ import { registerAs } from '@nestjs/config';
 import { CookieOptions } from 'express';
 
 export default registerAs<CookieOptions>('refreshToken', () => ({
-  expires: new Date(
-    Date.now() + parseInt(process.env.REFRESH_TOKEN_LIFETIME) * 60 * 1000,
-  ),
   maxAge: parseInt(process.env.REFRESH_TOKEN_LIFETIME) * 60 * 1000,
   httpOnly: true,
-  secure: true,
+  secure: false,
   sameSite: 'none',
 }));
