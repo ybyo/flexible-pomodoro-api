9186e72f3987c0a4b35c7693f5416a5c4a9af03b
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-01-10 12:14:42 +0900

    Add jwt guard

--- src/auth/guard/jwt-auth.guard.ts
+++ src/auth/guard/jwt-auth.guard.ts
@@ -1,5 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const result = (await super.canActivate(context)) as boolean;
+
+    return result;
+  }
+
+  handleRequest(err, user, info, context, status) {
+    // console.log(err, user, info, context, status);
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    // TODO: 자동 갱신 로직 구현
+    // } else if (info == 'TokenExpiredError: jwt expired') {
+    //   return info;
+    // }
+    return user;
+  }
+}
