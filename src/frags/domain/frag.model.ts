5bf4a8a4e901fc90e8901f5871e24cdc026c5316
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-01-15 21:51:28 +0900

    Add getters and setters to Frag class

--- src/frags/domain/frag.model.ts
+++ src/frags/domain/frag.model.ts
@@ -4,4 +4,44 @@ export class Frag {
   private _duration: number;
   private _count: number;
   private _color: string;
+
+  get id(): string {
+    return this._id;
+  }
+
+  set id(value: string) {
+    this._id = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get duration(): number {
+    return this._duration;
+  }
+
+  set duration(value: number) {
+    this._duration = value;
+  }
+
+  get count(): number {
+    return this._count;
+  }
+
+  set count(value: number) {
+    this._count = value;
+  }
+
+  get color(): string {
+    return this._color;
+  }
+
+  set color(value: string) {
+    this._color = value;
+  }
 }
