9cd623916ba2c8561c14fdc5627dd9188f04d684
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-01-21 21:05:39 +0900

    Clean up codes

--- src/stacks/interface/stacks.controller.ts
+++ src/stacks/interface/stacks.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-import { AuthService } from '@/auth/auth.service';
 import { JwtAuthGuard } from '@/auth/guard/jwt-auth.guard';
 import { Request } from 'express';
 import { CommandBus } from '@nestjs/cqrs';
@@ -11,16 +10,11 @@ import { Stacks } from '@/stacks/domain/stacks.model';
 
 @Controller('stacks')
 export class StacksController {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly commandBus: CommandBus,
-  ) {}
+  constructor(private readonly commandBus: CommandBus) {}
   @UseGuards(JwtAuthGuard)
   @Get('fetch')
   // TODO: 응답 타입 정의
   async fetch(@Req() req: Request) {
-    // 인증까지 완료된 상황
-    // TODO: 커맨드 필요(커맨드 핸들러, 커맨드 생성기)
     const user = req.user as JwtPayload & IUser;
 
     let stacks;
