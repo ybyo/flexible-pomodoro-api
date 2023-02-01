5549407e24448796af40a541fcb5b3910b2a1714
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-02-01 18:37:37 +0900

    Remove underscores in frag, stack models

--- src/frags/domain/frag.model.ts
+++ src/frags/domain/frag.model.ts
@@ -1,47 +1,61 @@
+import { plainToClass } from 'class-transformer';
+
 export class Frag {
-  private _id: string;
-  private _name: string;
-  private _duration: number;
-  private _count: number;
-  private _color: string;
+  id: string;
+  name: string;
+  duration: number;
+  count: number;
+  order: number;
+  color: string;
 
-  get id(): string {
-    return this._id;
+  constructor(frag: Partial<Frag>) {
+    if (frag) {
+      Object.assign(
+        this,
+        plainToClass(Frag, frag, {
+          excludeExtraneousValues: false,
+        }),
+      );
+    }
   }
 
-  set id(value: string) {
-    this._id = value;
-  }
-
-  get name(): string {
-    return this._name;
-  }
-
-  set name(value: string) {
-    this._name = value;
-  }
-
-  get duration(): number {
-    return this._duration;
-  }
-
-  set duration(value: number) {
-    this._duration = value;
-  }
-
-  get count(): number {
-    return this._count;
-  }
-
-  set count(value: number) {
-    this._count = value;
-  }
-
-  get color(): string {
-    return this._color;
-  }
-
-  set color(value: string) {
-    this._color = value;
-  }
+  // get id(): string {
+  //   return this._id;
+  // }
+  //
+  // set id(value: string) {
+  //   this._id = value;
+  // }
+  //
+  // get name(): string {
+  //   return this._name;
+  // }
+  //
+  // set name(value: string) {
+  //   this._name = value;
+  // }
+  //
+  // get duration(): number {
+  //   return this._duration;
+  // }
+  //
+  // set duration(value: number) {
+  //   this._duration = value;
+  // }
+  //
+  // get count(): number {
+  //   return this._count;
+  // }
+  //
+  // set count(value: number) {
+  //   this._count = value;
+  // }
+  //
+  // get color(): string {
+  //   return this._color;
+  // }
+  //
+  // set color(value: string) {
+  //   this._color = value;
+  // }
 }
