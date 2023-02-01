5831435a9b5f54872b43fe07878cdbd84ad2a086
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-02-01 18:42:43 +0900

    Fix parameter types, arguments

--- src/stacks/application/command/impl/save-stacks.command.ts
+++ src/stacks/application/command/impl/save-stacks.command.ts
@@ -2,5 +2,5 @@ import { ICommand } from '@nestjs/cqrs';
 import { Stacks } from '@/stacks/domain/stacks.model';
 
 export class SaveStacksCommand implements ICommand {
-  constructor(readonly id: string, readonly stacks: Stacks[]) {}
+  constructor(readonly id: string, readonly stacks: Stacks) {}
 }
