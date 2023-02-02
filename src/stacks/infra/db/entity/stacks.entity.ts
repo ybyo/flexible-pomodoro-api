a83a653fffc32c03a94fc2c20d911de71ae8ce94
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-02-02 22:20:32 +0900

    Remove eager, add select option to entity columns

--- src/stacks/infra/db/entity/stacks.entity.ts
+++ src/stacks/infra/db/entity/stacks.entity.ts
@@ -7,11 +7,9 @@ import {
   OneToMany,
   PrimaryColumn,
   RelationId,
-  UpdateDateColumn,
 } from 'typeorm';
 import { UserEntity } from '@/users/infra/db/entity/user.entity';
 import { StacksToFragEntity } from '@/stacks/infra/db/entity/stacks-to-frag.entity';
-import { JoinColumn } from 'typeorm/browser';
 
 @Entity('Stacks')
 export class StacksEntity extends BaseEntity {
@@ -24,21 +22,20 @@ export class StacksEntity extends BaseEntity {
   @Column()
   count: number;
 
-  @CreateDateColumn()
+  @CreateDateColumn({ select: false })
   createdAt: Date;
 
-  @UpdateDateColumn()
+  @CreateDateColumn({ select: false })
   updatedAt: Date;
 
   @ManyToOne(() => UserEntity, (userEntity) => userEntity.stacks)
   user: UserEntity;
 
   @RelationId((stacksEntity: StacksEntity) => stacksEntity.user)
-  @Column({ nullable: true })
+  @Column({ nullable: true, select: false })
   userId: string;
 
   @OneToMany(() => StacksToFragEntity, (stacksToFrag) => stacksToFrag.stacks, {
-    eager: true,
     cascade: true,
   })
   stacksToFrag: StacksToFragEntity[];
