6bf1d66ddf37b784f0bf8149fdbe043e636cedd5
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-02-02 22:22:29 +0900

    Inject stacks-to-frag entity to stacks repository

--- src/stacks/infra/db/repository/stacks.repository.ts
+++ src/stacks/infra/db/repository/stacks.repository.ts
@@ -8,6 +8,7 @@ import { IStacksRepository } from '@/stacks/domain/istacks.repository';
 import { InjectMapper } from '@automapper/nestjs';
 import { Stacks } from '@/stacks/domain/stacks.model';
 import { DataSource, Repository } from 'typeorm';
+import { StacksToFragEntity } from '@/stacks/infra/db/entity/stacks-to-frag.entity';
 
 @Injectable()
 export class StacksRepository implements IStacksRepository {
@@ -16,6 +17,8 @@ export class StacksRepository implements IStacksRepository {
     private dataSource: DataSource,
     @InjectRepository(StacksEntity)
     private stackRepository: Repository<StacksEntity>,
+    @InjectRepository(StacksToFragEntity)
+    private stacksToFragRepository: Repository<StacksToFragEntity>,
   ) {}
   // TODO: 리턴 타입 수정
   async fetchStack(id: string): Promise<any> {
@@ -59,6 +62,8 @@ export class StacksRepository implements IStacksRepository {
         });
         const result = StacksEntity.create(formatResult[0]);
 
+        // 기존에 DB에 저장되어있는 같은 stacksId 관련 정보 제거
+        await this.stacksToFragRepository.delete({ stacksId: stacks.id });
         await manager.save(result);
       });
     } catch (err) {
