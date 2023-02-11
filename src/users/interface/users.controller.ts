5c086a694982e8ebccdf0b56acba7effaa736f01
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-02-11 22:42:04 +0900

    Change return data in verify email handler

--- src/users/interface/users.controller.ts
+++ src/users/interface/users.controller.ts
@@ -1,15 +1,16 @@
 import {
   Controller,
+  Get,
   Inject,
   Logger,
   LoggerService,
-  Post,
   Query,
+  Req,
+  Res,
 } from '@nestjs/common';
 import { AuthService } from '@/auth/auth.service';
 import { CommandBus } from '@nestjs/cqrs';
 import { VerifyEmailCommand } from '@/users/application/command/impl/verify-email.command';
-import { VerifyEmailDto } from './dto/verify-email.dto';
 
 @Controller('user')
 export class UsersController {
@@ -19,13 +20,23 @@ export class UsersController {
     private commandBus: CommandBus,
   ) {}
 
-  // TODO: 계정 인증 시, 로그에 액세스 토큰이 프론트로 전송됐는지 여부와 생성된 계정 정보만 출력하도록 수정
-  @Post('verify-email')
-  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
-    const { signupVerifyToken } = dto;
+  @Get('verify-email')
+  async verifyEmail(
+    @Req() req,
+    @Query() query,
+    @Res({ passthrough: true }) res,
+  ): Promise<string> {
+    const { signupVerifyToken } = query;
 
     const command = new VerifyEmailCommand(signupVerifyToken);
 
-    return this.commandBus.execute(command);
+    const result = await this.commandBus.execute(command);
+
+    // if ('success' in result && result.success === false) {
+    //   return result;
+    // } else {
+    //   return result;
+    // }
+    return result;
   }
 }
