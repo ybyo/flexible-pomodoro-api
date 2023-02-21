37d4ec04e0bdad133b95fde3bd119d5f00f95ffa
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-02-21 17:32:05 +0900

    Remove required validation field for sendgrid

--- src/config/validationSchema.ts
+++ src/config/validationSchema.ts
@@ -1,9 +1,9 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-  EMAIL_SERVICE: Joi.string().required(),
-  EMAIL_AUTH_USER: Joi.string().required(),
-  EMAIL_AUTH_PASSWORD: Joi.string().required(),
+  EMAIL_SERVICE: Joi.string(),
+  EMAIL_AUTH_USER: Joi.string(),
+  EMAIL_AUTH_PASSWORD: Joi.string(),
   BASE_URL: Joi.string().required(),
   FRONT_PORT: Joi.number().required(),
   API_PORT: Joi.number().required(),
