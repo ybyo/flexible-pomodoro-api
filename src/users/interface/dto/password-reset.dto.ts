6fa75b1462d93ab4a7a78f4cd701a243148c66fb
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-03-27 06:49:21 +0100

    PT-8 #comment Implement DTO to verify if the two entered passwords match

---
+++ src/users/interface/dto/password-reset.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { MatchPassword } from '@/utils/decorators/match-password.decorator';
+
+export class PasswordResetDto {
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/^.{8,32}$/)
+  readonly password: string;
+
+  // TODO: Test 추가
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @MatchPassword('password', { message: 'Password does not match.' })
+  passwordConfirm: string;
+}
