68a4be65e8f2141fe5e13b433a2eb618932491ee
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-04-18 06:21:23 +0100

    PT-82 #comment feat: change validate logic and add mysql User table update

--- src/users/common/strategy/redis-token.strategy.ts
+++ src/users/common/strategy/redis-token.strategy.ts
@@ -1,22 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-custom';
 
 import { RedisService } from '@/redis/redis.service';
+import { IUserRepository } from '@/users/domain/repository/iuser.repository';
+import { UserRepository } from '@/users/infra/db/repository/user.repository';
 
 @Injectable()
 export class RedisTokenStrategy extends PassportStrategy(
   Strategy,
   'redis-token',
 ) {
-  constructor(private readonly redisService: RedisService) {
+  constructor(
+    private readonly redisService: RedisService,
+    @Inject('UserRepository') private readonly userRepository: IUserRepository,
+  ) {
     super();
   }
 
   async validate(req: Request): Promise<boolean> {
     const token = 'query' in req ? req.query['signupVerifyToken'] : null;
-    const savedToken = await this.redisService.getValue(`verifyEmail:${token}`);
+    const id = await this.redisService.getValue(`verifyEmail:${token}`);
 
-    return savedToken === '1';
+    if (id === null) {
+      throw new BadRequestException('Already verified email');
+    }
+
+    await this.redisService.deleteValue(`verifyEmail:${token}`).then(() => {
+      this.userRepository.updateUser({ id }, { signupVerifyToken: null });
+    });
+
+    return true;
   }
 }
