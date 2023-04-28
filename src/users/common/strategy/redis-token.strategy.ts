4c9f84b97b20a1ab7de9ed6eaa862e01982cfb4b
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-04-28 13:56:46 +0100

    PT-110 #comment feat: implement transaction support in Redis client and TypeORM for secure token handling

--- src/users/common/strategy/redis-token.strategy.ts
+++ src/users/common/strategy/redis-token.strategy.ts
@@ -1,10 +1,17 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { Strategy } from 'passport-custom';
 
 import { RedisService } from '@/redis/redis.service';
 import { IUserRepository } from '@/users/domain/repository/iuser.repository';
+import { User } from '@/users/domain/user.model';
+import { UserEntity } from '@/users/infra/db/entity/user.entity';
 import { UserRepository } from '@/users/infra/db/repository/user.repository';
 
 @Injectable()
@@ -29,26 +36,42 @@ export class RedisTokenStrategy extends PassportStrategy(
       throw new BadRequestException(`Invalid request`);
     }
 
-    let key = '';
-    let token = '';
+    let key;
+    let token;
 
     if (raw === req.cookies.resetPasswordToken) {
       key = 'resetPasswordToken';
       token = raw;
     } else {
-      key = Object.keys(raw)[0] as string;
-      token = Object.values(raw)[0] as string;
+      key = Object.keys(raw)[0];
+      token = Object.values(raw)[0];
     }
 
-    const id = await this.redisService.getValue(`${key}:${token}`);
+    const redis = await this.redisService.getClient();
+    const multi = redis.multi();
+    multi.del(`${key}:${token}`);
 
-    if (id === null) {
-      throw new BadRequestException(`${key} is already used or invalid token`);
-    }
+    await this.userRepository
+      .findBySignupVerifyToken(token)
+      .then(async (user: Pick<User, 'id'> | null): Promise<void> => {
+        if (user !== null) {
+          await this.userRepository
+            .getDataSource()
+            .transaction(async (manager) => {
+              await manager.update(
+                UserEntity,
+                { id: user.id },
+                { signupVerifyToken: null },
+              );
 
-    await this.redisService.deleteValue(`${key}:${token}`).then(() => {
-      this.userRepository.updateUser({ id }, { [key]: null });
-    });
+              await multi.exec();
+            });
+        }
+      })
+      .catch(async (err) => {
+        await multi.discard();
+        throw new InternalServerErrorException('Cannot verify token');
+      });
 
     return true;
   }
