e5b6f0ab6643eed875d6eb67c3e39d64a41abf23
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-04-29 02:26:25 +0100

    PT-110 #comment refactor: change token generation method from JWT to ULID for security reasons

--- src/auth/command/handler/register-user.handler.ts
+++ src/auth/command/handler/register-user.handler.ts
@@ -33,11 +33,7 @@ export class RegisterUserHandler
     if (user !== null) throw new BadRequestException('Duplicate email');
 
     const newUserId = ulid();
-    const signupVerifyToken = await this.authService.issueJWT({
-      id: newUserId,
-      userName,
-      email,
-    });
+    const signupVerifyToken = ulid();
 
     const newUser = new User({
       ...command,
