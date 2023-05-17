d00ddb29a4697732eaf5eb87b8a0ba331d0ff5a6
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-05-17 01:20:15 +0100

    PT-142 #comment feat: add logger provider to email module

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 
 import { EmailService } from './email.service';
 
 @Module({
-  providers: [EmailService],
+  providers: [EmailService, Logger],
   exports: [EmailService],
 })
 export class EmailModule {}
