5a51367d5a3b3173d46a4bfdf0ab9b877345d0b3
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-05-17 03:15:56 +0100

    feat: refactor `User` model and add new model class to improve security and data handling

--- src/users/domain/user.model.ts
+++ src/users/domain/user.model.ts
@@ -1,103 +1,28 @@
-import { plainToClass } from 'class-transformer';
+import { AGenerateUserJwt } from '@/shared/abstract/generate-user-jwt.base';
 
-export class User {
+export class UserWithoutPassword extends AGenerateUserJwt {
   id: string;
-  userName: string;
   email: string;
-  password: string;
-  signupToken: string;
+  name: string;
   newEmail: string;
-  refreshToken: string;
-  resetPasswordToken: string;
+  signupToken: string;
+  changePasswordToken: string;
+  changeEmailToken: string;
+}
 
-  constructor(user: Partial<User>) {
-    if (user) {
-      Object.assign(
-        this,
-        plainToClass(User, user, {
-          excludeExtraneousValues: false,
-        }),
-      );
-    }
-  }
+export class User extends UserWithoutPassword {
+  id: string;
+  email: string;
+  name: string;
+  password: string;
+  newEmail: string;
+  signupToken: string;
+  changePasswordToken: string;
+  changeEmailToken: string;
+}
 
-  // get id(): Readonly<string> {
-  //   return this._id;
-  // }
-  //
-  // set id(id: string) {
-  //   this._id = id;
-  // }
-  //
-  // get userName(): Readonly<string> {
-  //   return this._userName;
-  // }
-  //
-  // set userName(userName: string) {
-  //   this._userName = userName;
-  // }
-  //
-  // get email(): Readonly<string> {
-  //   return this._email;
-  // }
-  //
-  // set email(email: string) {
-  //   this._email = email;
-  // }
-  //
-  // get password(): Readonly<string> {
-  //   return this._password;
-  // }
-  //
-  // set password(hashedPassword: string) {
-  //   this._password = hashedPassword;
-  // }
-  //
-  // get signupToken(): Readonly<string> {
-  //   return this._SignupToken;
-  // }
-  //
-  // set signupToken(signupToken: string) {
-  //   this._SignupToken = signupToken;
-  // }
-  //
-  // get refreshToken(): Readonly<string> {
-  //   return this._refreshToken;
-  // }
-  //
-  // set refreshToken(refreshToken: string) {
-  //   this._refreshToken = refreshToken;
-  // }
-  //
-  // get resetPasswordToken(): Readonly<string> {
-  //   return this._resetPasswordToken;
-  // }
-  //
-  // set resetPasswordToken(resetPasswordToken: string) {
-  //   this._resetPasswordToken = resetPasswordToken;
-  // }
-  //
-  // get isVerified(): Readonly<boolean> {
-  //   return this._isVerified;
-  // }
-  //
-  // set isVerified(isVerified: boolean) {
-  //   this._isVerified = isVerified;
-  // }
-  //
-  // get isLoggedin(): Readonly<boolean> {
-  //   return this._isLoggedin;
-  // }
-  //
-  // set isLoggedin(isLoggedin: boolean) {
-  //   this._isLoggedin = isLoggedin;
-  // }
-  //
-  // get isActive(): Readonly<boolean> {
-  //   return this._isActive;
-  // }
-  //
-  // set isActive(isActive: boolean) {
-  //   this._isActive = isActive;
-  // }
+export class UserJwt extends AGenerateUserJwt {
+  id: string;
+  email: string;
+  name: string;
 }
