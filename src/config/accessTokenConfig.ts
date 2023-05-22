05d0094e8fc8e6d8950dfa01c2b21bd28742105b
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-05-22 05:29:51 +0100

    refactor: token related environment variable names

--- src/config/accessTokenConfig.ts
+++ src/config/accessTokenConfig.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { CookieOptions } from 'express';
 
 export default registerAs<CookieOptions>('accessToken', () => ({
-  maxAge: +process.env.ACCESS_TOKEN_LIFETIME * 24 * 60 * 60 * 1000,
+  maxAge: +process.env.ACCESS_LIFETIME * 24 * 60 * 60 * 1000,
   httpOnly: true,
   secure: true,
   sameSite: 'lax',
--- src/config/refreshTokenConfig.ts
+++ src/config/refreshTokenConfig.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { CookieOptions } from 'express';
 
 export default registerAs<CookieOptions>('refreshToken', () => ({
-  maxAge: +process.env.REFRESH_TOKEN_LIFETIME * 24 * 60 * 60 * 1000,
+  maxAge: +process.env.REFRESH_LIFETIME * 24 * 60 * 60 * 1000,
   httpOnly: true,
   secure: true,
   sameSite: 'lax',
