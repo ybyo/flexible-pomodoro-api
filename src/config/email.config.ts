b7815fa69ff98e4c00cfb049eb442e45ebf9e1e5
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-05-29 14:09:17 +0100

    refactor: reformat code

--- src/config/email.config.ts
+++ src/config/email.config.ts
@@ -14,11 +14,11 @@ interface IEmailConfig {
 
 export default registerAs('email', (): IEmailConfig => {
   const emailConfig: IEmailConfig = {
-    host: process.env.FRONT_URL || '127.0.0.1',
+    host: process.env.HOST_URL || '127.0.0.1',
     api_port: +process.env.API_PORT || 3000,
     front_port: +process.env.FRONT_PORT || 4000,
     auth: {
-      sgMailKey: process.env.SENDGRID_API,
+      sgMailKey: process.env.TEST ? null : process.env.SENDGRID_API,
       testService: process.env.EMAIL_SERVICE,
       testUser: process.env.EMAIL_AUTH_USER,
       testPassword: process.env.EMAIL_AUTH_PASSWORD,
