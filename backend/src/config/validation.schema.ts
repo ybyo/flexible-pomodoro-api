874ef2a7ef7b15ed646ab82e98f7e088c263799e
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-06-02 13:37:42 +0100

    refactor: refactor validationSchema for improved functionality based on NODE_ENV

--- backend/src/config/validation.schema.ts
+++ backend/src/config/validation.schema.ts
@@ -1,24 +1,12 @@
 import * as Joi from 'joi';
 
-export const validationSchema = Joi.object({
-  EMAIL_SERVICE: Joi.string().when('NODE_ENV', {
-    is: Joi.equal('development'),
-    then: Joi.required(),
-    otherwise: Joi.optional(),
-  }),
-  EMAIL_AUTH_USER: Joi.string().when('NODE_ENV', {
-    is: Joi.equal('development'),
-    then: Joi.required(),
-    otherwise: Joi.optional(),
-  }),
-  EMAIL_AUTH_PASSWORD: Joi.string().when('NODE_ENV', {
-    is: Joi.equal('development'),
-    then: Joi.required(),
-    otherwise: Joi.optional(),
-  }),
-  SENDGRID_API: Joi.string().when('NODE_ENV', {
-    is: Joi.not('development'),
-    then: Joi.required(),
-    otherwise: Joi.optional(),
-  }),
-});
+export const validationSchema =
+  process.env.NODE_ENV === 'development'
+    ? Joi.object({
+        EMAIL_SERVICE: Joi.string().required(),
+        EMAIL_AUTH_USER: Joi.string().required(),
+        EMAIL_AUTH_PASSWORD: Joi.string().required(),
+      })
+    : Joi.object({
+        SENDGRID_KEY: Joi.string().required(),
+      });
