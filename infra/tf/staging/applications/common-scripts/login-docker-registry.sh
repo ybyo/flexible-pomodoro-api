0aa9effb7ce34b3796a67d8f5f53dad6d1ba1edd
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-06-17 15:44:27 +0100

    refactor: refactor environments
    Signed-off-by: Yi Doyoon <yidoyoon@yidoyoon.com>

--- infra/tf/staging/applications/common-scripts/login-docker-registry.sh
+++ infra/tf/staging/applications/common-scripts/login-docker-registry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 registry_url="${1:-${REGISTRY_URL}}"
-username="${2:-${REGISTRY_ID}}"
-password="${3:-${REGISTRY_PASSWORD}}"
+registry_id="${2:-${REGISTRY_ID}}"
+registry_password="${3:-${REGISTRY_PASSWORD}}"
 
-echo "$password" | docker login -u "$username" "$registry_url" --password-stdin
+echo "$registry_password" | docker login -u "$registry_id" "$registry_url" --password-stdin
