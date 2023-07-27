c8ee06a6fd307c449f1b0de786dcd49c4600ffb3
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-07-27 15:52:44 +0100

    chore(deps): migrate package manager npm to pnpm
    Signed-off-by: Yi Doyoon <yidoyoon@yidoyoon.com>

--- infra/tf/production/applications/backend/shell-scripts/build-push-registry.sh
+++ infra/tf/production/applications/backend/shell-scripts/build-push-registry.sh
@@ -2,6 +2,6 @@
 
 cd ../../../../
 
-PLATFORM=linux/$1 npm run production:compose:build nginx-pt
+PLATFORM=linux/$1 pnpm run production:compose:build nginx-pt
 
 docker push "$2/pipe-timer-frontend:$3"
