aa9542e7889e6b875d5fa8cc997d06dfe12fdd66
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-08-13 05:29:23 +0100

    refactor: refactor cleanup script
    Signed-off-by: Yi Doyoon <yidoyoon@yidoyoon.com>

--- infra/tf/staging/applications/common-scripts/cleanup.sh
+++ infra/tf/staging/applications/common-scripts/cleanup.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-touch cleanup_tunnel.sh
-cat > cleanup_tunnel.sh << EOF
-if command -v cloudflared >/dev/null 2>&1; then
-  sudo systemctl stop cloudflared
-fi
-EOF
-chmod +x cleanup_tunnel.sh
-mv cleanup_tunnel.sh /home/ubuntu
-
 touch cleanup_tunnel.service
 cat > cleanup_tunnel.service << EOF
 [Unit]
@@ -18,7 +9,7 @@ Before=shutdown.target reboot.target halt.target
 
 [Service]
 Type=oneshot
-ExecStart=/home/ubuntu/cleanup_tunnel.sh
+ExecStart=sudo systemctl stop cloudflared
 TimeoutStartSec=0
 
 [Install]
@@ -26,4 +17,3 @@ WantedBy=halt.target reboot.target shutdown.target
 EOF
 sudo mv cleanup_tunnel.service /etc/systemd/system/cleanup_tunnel.service
 sudo systemctl enable cleanup_tunnel.service
-sudo systemctl start cleanup_tunnel.service
