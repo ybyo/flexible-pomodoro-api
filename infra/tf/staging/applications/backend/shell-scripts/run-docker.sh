0be9e05d2b6b07776b3637249f1f91d1a092444b
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-08-15 11:47:46 +0100

    fix: fix run-docker script
    Signed-off-by: Yi Doyoon <yidoyoon@yidoyoon.com>

--- infra/tf/staging/applications/backend/shell-scripts/run-docker.sh
+++ infra/tf/staging/applications/backend/shell-scripts/run-docker.sh
@@ -2,27 +2,27 @@
 
 echo "${registry_password}" | docker login -u "${registry_id}" "${registry_url}" --password-stdin
 
-docker network create pipe-timer || { echo 'Failed to create network'; exit 1; }
+docker network create pipe-timer || { echo 'Failed to create network'; }
 
 docker run -itd \
   --name promtail \
-  --network=pipe-timer \
+  --network pipe-timer \
   -v "${cicd_path}"/promtail-config.yml:/mnt/config/promtail-config.yml \
   -v /var/log:/var/log \
   -v /var/run/docker.sock:/var/run/docker.sock \
   grafana/promtail:2.8.0 \
-  --config.file=/mnt/config/promtail-config.yml || { echo 'Failed to run promtail'; exit 1; }
+  --config.file /mnt/config/promtail-config.yml || { echo 'Failed to run promtail'; }
 
 docker run -itd \
   --name nestjs \
   -p "${api_port}":"${api_port}" \
   --env-file "${cicd_path}"/env/."${env}".env \
-  --network=pipe-timer \
-  --network-alias=nestjs \
-  -v "$cicd_path"/certs:/certs:ro \
-  -v "$cicd_path"/env:/env:ro \
+  --network pipe-timer \
+  --network-alias nestjs \
+  -v "${cicd_path}"/certs:/certs:ro \
+  -v "${cicd_path}"/env:/env:ro \
   --restart on-failure \
-  "$registry_url"/pipe-timer-backend:"$env" || { echo 'Failed to run nestjs'; exit 1; }
+  "${registry_url}"/pipe-timer-backend:"${env}" || { echo 'Failed to run nestjs'; }
 
 docker run -d \
   --name node-exporter \
@@ -32,19 +32,19 @@ docker run -d \
   -v "${cicd_path}"/web-config-exporter.yml:/web-config-exporter.yml:ro \
   -v "${cicd_path}"/certs:"${cicd_path}"/certs:ro \
   quay.io/prometheus/node-exporter:latest \
-  --web.config.file=web-config-exporter.yml \
-  --path.rootfs=/host || { echo 'Failed to run node-exporter'; exit 1; }
+  --web.config.file web-config-exporter.yml \
+  --path.rootfs /host || { echo 'Failed to run node-exporter'; }
 
 docker run -itd \
   --name nginx \
   -p 443:443 \
-  --env-file "${cicd_path}"/env/."$env".env \
-  --network=pipe-timer \
-  --network-alias=nginx \
+  --env-file "${cicd_path}"/env/."${env}".env \
+  --network pipe-timer \
+  --network-alias nginx \
   -v "${cicd_path}"/nginx.conf:/etc/nginx/templates/nginx.conf.template:ro \
   -v "${cicd_path}"/certs:/etc/nginx/certs:ro \
-  --add-host=host.docker.internal:host-gateway \
-  nginx || { echo 'Failed to run nginx'; exit 1; }
+  --add-host host.docker.internal:host-gateway \
+  nginx || { echo 'Failed to run nginx'; }
 
 sleep 5
 
