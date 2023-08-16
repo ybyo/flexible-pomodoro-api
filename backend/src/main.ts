3d5c67cc7d8fb01e748406f534d379fd1e466bb0
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-08-16 05:31:02 +0100

    feat: add cors url
    Signed-off-by: Yi Doyoon <yidoyoon@yidoyoon.com>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -102,6 +102,7 @@ async function bootstrap() {
     origin: [
       `https://${process.env.HOST_URL}`,
       `https://${process.env.HOST_URL}:${process.env.FRONT_PORT_0}`,
+      `https://${process.env.HOST_URL}:${process.env.FRONT_PORT_2}`,
     ],
     credentials: true,
     optionsSuccessStatus: 200,
