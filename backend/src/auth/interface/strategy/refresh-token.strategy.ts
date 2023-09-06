2232f1adb222d3a867fc6998e8a37aa4950765b5
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-09-06 08:10:09 +0100

    test: add test for 'RefreshTokenGuard'

--- backend/src/auth/interface/strategy/refresh-token.strategy.ts
+++ backend/src/auth/interface/strategy/refresh-token.strategy.ts
@@ -3,33 +3,22 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { Strategy } from 'passport-custom';
 
-import { RedisTokenService } from '@/redis/redis-token.service';
+import { RedisAuthService } from '@/redis/redis-auth.service';
 
 @Injectable()
 export class RefreshTokenStrategy extends PassportStrategy(
   Strategy,
   'refresh-token'
 ) {
-  constructor(private redisService: RedisTokenService) {
+  constructor(private redisAuthService: RedisAuthService) {
     super();
   }
 
   async validate(req: Request): Promise<any> {
-    console.log(req);
     if (req.cookies.hasOwnProperty('refreshToken') === false) {
       throw new UnauthorizedException('No refreshToken');
+    } else {
+      return this.redisAuthService.checkLoggedIn(req);
     }
-
-    const refreshToken = req.cookies['refreshToken']
-      .split(':')[1]
-      .split('.')[0];
-    const sessionString = await this.redisService.getValue(
-      `sess:${refreshToken}`
-    );
-    const sessionObject = JSON.parse(sessionString);
-
-    return sessionObject.hasOwnProperty('passport')
-      ? sessionObject.passport.user
-      : false;
   }
 }
