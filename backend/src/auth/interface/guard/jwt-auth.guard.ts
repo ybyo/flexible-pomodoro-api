715b558c38accc7db88a35708bdf2346896b2888
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-09-06 05:26:25 +0100

    refactor: reformat code

--- backend/src/auth/interface/guard/jwt-auth.guard.ts
+++ backend/src/auth/interface/guard/jwt-auth.guard.ts
@@ -4,8 +4,6 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   async canActivate(ctx: ExecutionContext): Promise<boolean> {
-    const result = (await super.canActivate(ctx)) as boolean;
-
-    return result;
+    return (await super.canActivate(ctx)) as boolean;
   }
 }
