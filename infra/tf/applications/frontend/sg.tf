c3086cf922cbc4e95e2459de3d732c92459fe1e1
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-09-20 15:46:43 +0100

    feat: separate `aws_security_group` into `aws_security_group_rule`
    Signed-off-by: Yi Doyoon <yidoyoon@yidoyoon.com>

--- infra/tf/applications/frontend/sg.tf
+++ infra/tf/applications/frontend/sg.tf
@@ -6,124 +6,20 @@ locals {
 resource "aws_security_group" "ssh" {
   name   = "sg_ssh_frontend_${terraform.workspace}"
   vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    cidr_blocks = compact([
-      "${data.http.ip.response_body}/32", "${local.envs["DEV_SERVER"]}/32",
-      local.production_cidr, local.staging_cidr
-    ])
-  }
-
-  dynamic "ingress" {
-    for_each = data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks
-    content {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = [ingress.value]
-    }
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_security_group" "node_exporter" {
   name   = "sg_node_exporter_frontend_${terraform.workspace}"
   vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
-
-  dynamic "ingress" {
-    for_each = data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks
-    content {
-      from_port   = local.envs["NODE_EXPORTER_PORT"]
-      to_port     = local.envs["NODE_EXPORTER_PORT"]
-      protocol    = "tcp"
-      cidr_blocks = [ingress.value]
-    }
-  }
-
-  ingress {
-    from_port   = local.envs["NODE_EXPORTER_PORT"]
-    to_port     = local.envs["NODE_EXPORTER_PORT"]
-    protocol    = "tcp"
-    cidr_blocks = ["${data.http.ip.response_body}/32"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_security_group" "https" {
   name   = "sg_https_frontend_${terraform.workspace}"
   vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
-
-  dynamic "ingress" {
-    for_each = data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks
-    content {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = [ingress.value]
-    }
-  }
-
-  ingress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-    cidr_blocks = compact([
-      "${data.http.ip.response_body}/32", "${local.envs["DEV_SERVER"]}/32",
-      local.production_cidr, local.staging_cidr
-    ])
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_security_group" "public_dns" {
   name   = "sg_dns_frontend_${terraform.workspace}"
   vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
-
-  dynamic "ingress" {
-    for_each = data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks
-    content {
-      from_port   = 53
-      to_port     = 53
-      protocol    = "tcp"
-      cidr_blocks = [ingress.value]
-    }
-  }
-
-  dynamic "ingress" {
-    for_each = data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks
-    content {
-      from_port   = 53
-      to_port     = 53
-      protocol    = "udp"
-      cidr_blocks = [ingress.value]
-    }
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
+
